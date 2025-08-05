baab807ca4e0591953eb3255d47af58ceea49a08
MunibOzdemir <144024003+MunibOzdemir@users.noreply.github.com>
2025-08-05 11:39:04 +0200

    Added function to fetch .tif path

--- src/tools.py
+++ src/tools.py
@@ -6,3 +6,17 @@ def get_geojson_path(filename):
     data_file_path = current_dir.parent / 'data' / filename
 
     return data_file_path
+
+def get_tif_path(filename=None, ndvi=True):
+    """
+    Get the path to the most recent .tif file in the 'data' directory.
+    If filename is provided, return that specific file path.
+    If ndvi is True, return the NDVI tif file path.
+    """
+    current_dir = Path(__file__).resolve().parent
+    data_dir = current_dir.parent / 'data'
+    if filename:
+        return data_dir / filename
+    if ndvi:
+        return data_dir / 'ndvi.tif'
+    return data_dir / 'latest.tif'
